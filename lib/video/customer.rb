8a749d893193ac4e753bfa9d47d2f2e34e46d440
Jeff L <jeff@langrsoft.com>
2021-01-16 07:58:57 -0700

    comments in place

--- lib/video/customer.rb
+++ lib/video/customer.rb
@@ -11,34 +11,37 @@ class Customer
   end
 
   def statement
-    total_amount = 0
-    frequent_renter_points = 0
+    total_amount, frequent_renter_points = 0, 0
     result = "Rental record for #{@name}\n"
-    for rental in @rentals
+    for element in @rentals
       this_amount = 0
-      case rental.movie.price_code
+
+      # determine amounts for each line
+      case element.movie.price_code
         when Movie::REGULAR
           this_amount += 2
-          if rental.days_rented > 2
-            this_amount += (rental.days_rented - 2) * 1.5
+          if element.days_rented > 2
+            this_amount += (element.days_rented - 2) * 1.5
           end
         when Movie::NEW_RELEASE
-          this_amount += rental.days_rented * 3
+          this_amount += element.days_rented * 3
         when Movie::CHILDRENS
           this_amount += 1.5
-          if rental.days_rented > 3
-            this_amount += (rental.days_rented - 3) * 1.5
+          if element.days_rented > 3
+            this_amount += (element.days_rented - 3) * 1.5
           end
         else
           # do nothing for a while
       end
 
+      # add frequent renter points
       frequent_renter_points += 1
-      if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
+      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
         frequent_renter_points += 1
       end
 
-      result += "\t#{rental.movie.title}\t#{this_amount}\n"
+      # add footer lines
+      result += "\t#{element.movie.title}\t#{this_amount}\n"
       total_amount += this_amount
     end
 
